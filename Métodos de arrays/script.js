af6f31ef205f03c4e520015139f1c5f5552fd34a
letici0costa <109427133+letici0costa@users.noreply.github.com>
2023-04-10 12:20:58 -0300

    Add files via upload

---
+++ Métodos de arrays/script.js
@@ -0,0 +1,87 @@
+//const variableName = [];
+
+//Exemplo 01 - Push
+//const users = ["Ana", "Fernando", "Rafael", "Clara", "Maria"];
+//users.push("Rogers");
+
+//console.log(users);
+
+//Exemplo 02 - Método Pop
+
+//const person = ["Felipe", 30, "São Paulo", "Masculino", "22233366689"]
+//person.pop();
+
+//console.log(person);
+
+//Exemplo 03 - Método shift
+//const fruits = ["Apple", "Orange", "Banana", "Pear"];
+//fruits.shift();
+
+//console.log(fruits);
+
+//Exemplo 04 - Método unshift
+//const jobs = ["Fronted", "Backend", "DevOps", "FullStack"];
+//jobs.unshift("Design");
+
+//console.log(jobs);
+
+//Exemplo 05 - Método indexOf
+//const jobs = ["Frontend", "Backend", "DevOps", "FullStack"]; // obs: O seu valor é tirado da sua posição, como 0, 1 ,2, 3...
+//let indexOf = jobs.indexOf("Frontend");
+
+//console.log(indexOf)
+
+//Método indexOf
+//const fruits = ["Apple", "Orange", "Banana", "Pear"];
+//let indexOfSearch = fruits.indexOf("Orange", 3);
+
+//console.log(indexOfSearch);
+
+//Exemplo 06 - Método sort
+//const jobs = ["Frontend", "Backend", "DevOps", "FullStack"];
+//jobs.sort();
+
+//console.log(jobs);
+
+//Exemplo 07 -
+//const colors = ["Red", "Green", "White", "Black"];
+//let join = colors.join("+");
+
+//console.log(join);
+
+//ATIVIDADE!!!!!!!!
+//ATIVIDADE 01
+//const names = ["Letícia", "Jéssica", "Jennifer", "Jayne"];
+//let join = names.join("+ ");
+
+//console.log(join);
+
+//ATIVIDADE 02
+
+//const names = ["Melany", "Jennifer", "Nádia", "Bia", "Beatriz"];
+//names.sort();
+
+//console.log(names);
+
+//ATIVIDADE 03
+//function myFunction(a, b) {
+    //let array = [];
+
+    //if(a.indexOf(b) !== -1){
+        //array.push(b + a); // a + types = atypes
+        //return array;
+
+    //} else {
+        //array.push(a + b); // types + a = typesa
+       // return array;
+   // }
+
+// }
+ //console.log(myFunction("typesa", "a"))
+
+ //ATIVIDADE 04
+//const color = ["branco", "vermelho", "azul", "roza"]
+
+//color.shift();
+//console.log(color);
+
